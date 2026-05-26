0aac79a417baed2cea07a95a2e9acf8f9c64e117
Hakukale <shahi.reson17@gmai.com>
2026-05-26 17:56:26 +0545

    First Day at Skill Shikshya

---
+++ hello.py
@@ -0,0 +1,11 @@
+#First simple python program
+print('Hello World')
+
+#Variables
+value = 'Sakshyam'
+print(type(value))
+
+#Everything in python is made up of class
+
+if __name__=='__main__':
+    print("this is the main function")
