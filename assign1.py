0a97bfbfa6ddcc3c2148c8f4e3b98bac59c23955
Santosh B <sbohara579@gmail.com>
2026-06-08 17:11:28 +0545

    classwork

---
+++ assign1.py
@@ -0,0 +1,38 @@
+#find the second largst number in list
+lst = [1,2,9,3,4,5,8]
+
+largest = lst[0]
+second_largest = lst[1]
+
+for i in lst:
+    if i > largest:
+        largest = i
+        
+
+
+
+# maximum = max(lst)
+# lst.remove(maximum)
+# larg = lst[0]
+
+
+# for i in lst:
+#     # if i != larg:
+#         if i > larg:
+#             larg = i
+# maximum = larg
+
+# larg = lst[0]
+# for i in lst:
+#     if i != maximum:
+#         if i > larg:
+#             larg = i
+# supermax  = larg
+
+# print(maximum)
+
+# # lst.remove(maximum)
+# print(supermax)
+
+
+
