81c50ba3ab4b34c628d085a40db7812b3ed18479
Santosh B <sbohara579@gmail.com>
2026-06-08 22:20:04 +0545

    anagram task completed

--- assign1.py
+++ assign1.py
@@ -1,15 +1,17 @@
 #find the second largst number in list
-lst = [1,2,9,3,4,5,8]
+# lst = [1,2,9,3,4,5,8]
 
-largest = lst[0]
-second_largest = lst[1]
+# largest = lst[0]
+# second_largest = lst[1]
 
-for i in lst:
-    if i > largest:
-        largest = i
+# for i in lst:
+#     if i > largest:
+#         largest = i
         
 
-
+# #2nd way to do.
+# lst.sort()
+# print(lst[-2])
 
 # maximum = max(lst)
 # lst.remove(maximum)
@@ -36,3 +38,25 @@ for i in lst:
 
 
 
+#2. Check if two strings are ANAGRAMS(eg. 'listen' and 'silent')
+
+# first = input("Enter the first string: ")
+# second = input("Enter the secodn string: ")
+first = ('good')
+second = ('dodg')
+
+
+if len(first) == len(second):
+    anagram = 0
+    for i in first:
+        if first.count(i) == second.count(i):
+        # for j in second:
+        #     if first.count(i) == second.count(j):
+                anagram += 1
+        #         break
+            
+
+if (anagram == len(first)):
+    print("This is a anagram word!!!")
+else:
+    print("This is not an anagram word")
