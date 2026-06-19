7ed1172e277075d801bd08e22e9fa3301b41431f
Santosh B <sbohara579@gmail.com>
2026-06-19 16:14:11 +0545

    assignment completed today

--- oops/assignment/task1.py
+++ oops/assignment/task1.py
@@ -15,5 +15,34 @@ class Vehicle:
         return f'{self.brand} is stopping...'
     
 class Car(Vehicle):
-    def __init__(self, brand, color, ):
+    def __init__(self, brand, color, doors, seats):
         super().__init__(brand, color)
+        self.doors = doors
+        self.seats = seats
+
+    def car_info(self):
+        return f'{self.brand} has {self.color} color and {self.doors} doors'
+    
+    def get_seats(self):
+        return f"hello! I'm {self.brand}. I have {self.seats} seats"
+
+    
+
+class Bike(Vehicle):
+    def __init__(self, brand, color, wheels):
+        super().__init__(brand, color)
+        self.wheels = wheels
+
+    def intro(self):
+        return f"{self.brand} has {self.color} color and {self.wheels} wheels"
+    
+
+
+bike = Bike('TVS', 'black', 2)
+
+print(bike.start_engine())
+print(bike.intro())
+
+car = Car('Porsche', 'red', 2, 2)
+
+print(car.car_info())
